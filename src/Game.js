1fb2d79bcbdaaba72b1c49a7c7a65812fee52417
Rulo180 <rullencio180@gmail.com>
2018-05-20 17:46:25 -0300

    feat(Game component): Add css styles
    - Create variables for color palette.
    - Overwrite bootstrap styles for buttons.
    - Add style for
    modules on Game.

--- src/Game.js
+++ src/Game.js
@@ -1,53 +1,56 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import HumanGuess from './components/HumanGuess';
 import IAGuess from './components/IAGuess';
 
 class Game extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			mode: 'human',
-		}
-	}
-	__selectMode(mode) {
-		this.setState({
-			mode: mode,
-		});
-	}
-	__toggleSlide() {
-		this.setState({
-			slide: !(this.state.slide),
-		});
-	}
-	render() { 
-        return ( 
-			<div className="container">
-				<div className="game">
-					<header>
-						<h1 className="text-center">Guess the Number</h1>
-					</header>
-					<div className="row justify-content-center">
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: 'human',
+    };
+  }
+  __selectMode(mode) {
+    this.setState({
+      mode,
+    });
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="game">
+          <header>
+            <h1 className="game__title">Guess the Number</h1>
+          </header>
+          <div className="row justify-content-center">
+            <div className="game__mode-btns btn-group shadow" role="group">
+              <button
+                onClick={this.__selectMode.bind(this, "human")}
+                type="button"
+                className="game__mode-btn btn btn-secondary"
 						<div className="btn-group" role="group">
 							<button onClick={this.__selectMode.bind(this, 'human')} type="button" className="btn btn-secondary">Human</button>
 							<button onClick={this.__selectMode.bind(this, 'ia')} type="button" className="btn btn-secondary">IA</button>
-						</div>
-					</div>
-					<main className="row">
-						<div className="col">
-							{(this.state.mode === 'human')?
-								<HumanGuess/>
-								:
-								<IAGuess/>
-							}
-						</div>
-					</main>
-					<footer>
-						
-					</footer>
-				</div>
-		  	</div>
-		 )
-    }
+              >
+                Human
+              </button>
+              <button
+                onClick={this.__selectMode.bind(this, "ia")}
+                type="button"
+                className="game__mode-btn btn btn-secondary"
+              >
+                IA
+              </button>
+            </div>
+          </div>
+          <main className="row">
+            <div className="col">
+              {this.state.mode === "human" ? <HumanGuess /> : <IAGuess />}
+            </div>
+          </main>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Game;
